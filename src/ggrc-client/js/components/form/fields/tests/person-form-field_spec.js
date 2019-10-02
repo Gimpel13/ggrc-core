8e78eba02ada8da6c47e97852379076a70e79c7f
Gimpel13 <gimpelllll@gmail.com>
2019-10-03 01:56:43 +0300

    Add person collection to assessment template

--- src/ggrc-client/js/components/form/fields/tests/person-form-field_spec.js
+++ src/ggrc-client/js/components/form/fields/tests/person-form-field_spec.js
@@ -5,6 +5,7 @@
 
 import {getComponentVM} from '../../../../../js_specs/spec-helpers';
 import Component from '../person-form-field';
+import canMap from 'can-map';
 
 describe('person-form-field component', function () {
   'use strict';
@@ -16,50 +17,40 @@ describe('person-form-field component', function () {
     viewModel.attr('fieldId', 1);
   });
 
+  it('add a person and fiers valueChanged event' +
+   'if new person has other id', function () {
+    viewModel.attr('value', [{id: 1}]);
+    viewModel.dispatch.calls.reset();
+    viewModel.addPerson({selectedItem: {id: 2}});
+    const dispatchedObject = viewModel.dispatch.calls.allArgs()[0][0];
+    expect(dispatchedObject.value[0].id).toEqual(1);
+    expect(dispatchedObject.value[1].id).toEqual(2);
+  });
+
   it('does not fire valueChanged event' +
     ' on first value assignation', function () {
     viewModel.attr('value', undefined);
     expect(viewModel.dispatch).not.toHaveBeenCalled();
   });
 
-  it('sets the value of the input', function () {
-    viewModel.attr('value', 'test');
-    expect(viewModel.attr('inputValue')).toEqual('test');
-  });
-
-  it('does not fire valueChanged event if value wasn\'t changed', function () {
-    viewModel.attr('value', {});
-    viewModel.attr('inputValue', 1);
+  it('does not fire valueChanged event' +
+    'if new person has similar id as defined people', function () {
+    viewModel.attr('value', [{id: 1}, {id: 2}]);
     viewModel.dispatch.calls.reset();
-    viewModel.attr('inputValue', 1);
+    viewModel.addPerson({selectedItem: new canMap({id: 2})});
     expect(viewModel.dispatch).not.toHaveBeenCalled();
   });
 
-  it('fires valueChanged event on input value change', function () {
-    viewModel.attr('value', '');
-    viewModel.attr('inputValue', 1);
-    expect(viewModel.dispatch).toHaveBeenCalledWith({
-      type: 'valueChanged',
-      fieldId: 1,
-      value: viewModel.attr('inputValue'),
-    });
-    viewModel.attr('inputValue', 2);
-    expect(viewModel.dispatch).toHaveBeenCalledWith({
-      type: 'valueChanged',
-      fieldId: 1,
-      value: viewModel.attr('inputValue'),
-    });
-  });
-
-  it('unsets a person and fires valueChanged event', function () {
-    viewModel.attr('value', '');
-    viewModel.attr('inputValue', 'newValue');
+  it('remove a person and fires valueChanged event', function () {
+    const attributeObject1 = new canMap({id: 1});
+    const attributeObject2 = new canMap({id: 2});
+    viewModel.attr('value', [attributeObject1, attributeObject2]);
     viewModel.dispatch.calls.reset();
-    viewModel.unsetPerson(null, null, new Event('mock'));
+    viewModel.removePerson({person: {id: 2}}, null, new Event('mock'));
     expect(viewModel.dispatch).toHaveBeenCalledWith({
       type: 'valueChanged',
       fieldId: 1,
-      value: null,
+      value: [attributeObject1],
     });
   });
 });
