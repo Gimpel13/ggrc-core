669ae6967e691b0b7e471a3559593ed24ca1d6ac
Gimpel13 <gimpelllll@gmail.com>
2019-10-03 02:01:17 +0300

    Replace attibute_object with attribute_objects

--- src/ggrc-client/js/models/conflict-resolution/conflict-resolvers.js
+++ src/ggrc-client/js/models/conflict-resolution/conflict-resolvers.js
@@ -79,7 +79,6 @@ export function customAttributeResolver(
       currentValueItem,
       remoteValueItem,
       containerValueItem,
-      'attribute_object.id',
       'attribute_object');
 
     conflict = conflict || hasValueConflict || hasObjectConflict;
